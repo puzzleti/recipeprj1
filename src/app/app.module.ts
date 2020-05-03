0e2f37afadfded70cd34edbecd60bb0ba3bcf156
thimmen <thimmen@gmx.de>
2020-05-03 19:45:09 +0200

    early project structur commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    ShoppinglistComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shoppinglist/shoppinglist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppinglist',
+  templateUrl: './shoppinglist.component.html',
+  styles: []
+})
+export class ShoppinglistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
